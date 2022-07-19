ff98ca44f64ed16ffd57be47e9f86c3dd242b60d
naveen magnur <naveen magnur@magnur>
2022-07-19 16:03:29 +0530

    By sanjay

---
+++ src/test/java/CreateContactTest.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createContact() {
+		System.out.println("======contact created=====");
+	}
+	
+	@Test
+	public void editContact() {
+		System.out.println("=====edited contact======");
+	}
+}
